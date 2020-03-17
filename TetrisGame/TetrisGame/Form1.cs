79c1354ffea8ba0aa715e52598fce2fd2f397bce
kokarolos <56258663+kokarolos@users.noreply.github.com>
2020-03-18 01:17:19 +0200

    Add files via upload

---
+++ TetrisGame/TetrisGame/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TetrisGame
+{
+
+    public partial class Form1 : Form
+    {
+        readonly Rectangle cube = new Rectangle();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+            timer1.Interval = (1000 / 60);
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            cube.Draw(e); //recursive
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cube.Movement(); // <= Updates  timer1.Interval seconds
+            pictureBox1.Invalidate(); //<= timer1.Interval seconds it will re-draw the picturebox UPDATED surface
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {}
+    }
+}
---
+++ TetrisGame/TetrisGame/Rectangle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TetrisGame
+{
+    public class Rectangle : IDrawable
+    {
+        public int Xposition { get; set; } = 250; 
+        public int Yposition { get; set; } = 10; 
+        public int Width { get; set; } = 15;
+        public int Height { get; set; } = 15;
+        public Color Color { get; set; } = Color.BlanchedAlmond;
+        public List<Rectangle> cube { get; set; } = new List<Rectangle>();
+        public Rectangle()
+        {
+            GenerateCube();
+        }
+
+        public Rectangle(int Xposition, int Yposition)
+        {
+            this.Xposition = Xposition;
+            this.Yposition = Yposition;
+        }
+
+        public void Draw(PaintEventArgs e) //Modularity
+        {
+            foreach (var item in cube)
+            {
+                Graphics g = e.Graphics;
+                SolidBrush b = new SolidBrush(Color);
+                g.FillRectangle(b, item.Xposition, item.Yposition, item.Width, item.Height);
+            }
+        }
+        public void Movement()
+        {
+            foreach (var item in cube)
+            {
+                item.Yposition += item.Yposition + 20; //-> oo 
+            }
+        }
+
+        //Creates Initial Cube
+        private void GenerateCube()
+        {
+            cube.Add(new Rectangle(Xposition, Yposition)); //Initial Rectangle
+            for (int i = 1; i <= 3; i++)
+            {
+                cube.Add(new Rectangle(cube[0].Xposition + Width, cube[0].Yposition)); //Secondary Rectangle
+                if (i == 2)
+                {
+                    cube.Add(new Rectangle(cube[0].Xposition, cube[0].Yposition + Height));
+                }
+                if (i == 3)
+                {
+                    cube.Add(new Rectangle(cube[1].Xposition, cube[1].Yposition + Height));
+                }
+            }
+        }
+    }
+}
