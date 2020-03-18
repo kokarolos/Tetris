3864bd7b14b42ae2d7f3bd710b56442ff5d67d74
kokarolos <56258663+kokarolos@users.noreply.github.com>
2020-03-18 16:46:57 +0200

    Add files via upload
    -Seperation Of Concerns
    -Hero Class Added

---
+++ TetrisGame/Models/Concrete/Hero.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Models
+{
+    public class Hero : Rectangle, IDrawable
+    {
+        public List<Rectangle> rects { get; set; } = new List<Rectangle>();
+        public override Color Color { get; set; } = Color.Red;
+
+        public Hero()
+        {
+            GenerateHero();
+        }
+
+        public Hero(int Xposition, int Yposition) : base(Xposition, Yposition)
+        {
+            this.Xposition = Xposition;
+            this.Yposition = Xposition;
+        }
+
+        public void Draw(PaintEventArgs e)
+        {
+            foreach (var item in rects)
+            {
+                Graphics g = e.Graphics;
+                SolidBrush b = new SolidBrush(Color);
+                g.FillRectangle(b, item.Xposition, item.Yposition, item.Width, item.Height);
+            }
+        }
+
+        public void Movement()
+        {
+            foreach (var item in rects)
+            {
+                item.Yposition += item.Yposition + 1;
+            }
+        }
+
+        //Creates Initial Hero
+        private void GenerateHero()
+        {
+            for(int i =0; i<=4; i++)
+            {
+                rects.Add(new Rectangle(Xposition + i*10, Yposition + Width, Width, Height));
+            }
+        }
+    }
+}
--- TetrisGame/TetrisGame/Form1.cs
+++ TetrisGame/TetrisGame/Form1.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Models;
+using System;
 using System.Windows.Forms;
 
 namespace TetrisGame
@@ -14,27 +8,29 @@ namespace TetrisGame
     public partial class Form1 : Form
     {
         readonly Cube cube = new Cube();
-
+        readonly Hero hero = new Hero();
 
         public Form1()
         {
             InitializeComponent();
             // KeyDown += new KeyEventHandler(Form1_KeyDown);
             Timer timer1 = new Timer();
-            timer1.Interval = (1000/10);
-            timer1.Tick += timer1_Tick;
-            timer1.Start();
+            //timer1.Interval = (1000/10);
+            //timer1.Tick += timer1_Tick;
+            //timer1.Start();
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            cube.Draw(e); //recursive
+            cube.Draw(e); 
+            hero.Draw(e);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            cube.Movement(); // <= Updates  timer1.Interval seconds
-            pictureBox1.Invalidate(); //<= timer1.Interval seconds it will re-draw the picturebox UPDATED surface
+            hero.Movement();
+            cube.Movement(); 
+            pictureBox1.Invalidate(); 
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e) {}
