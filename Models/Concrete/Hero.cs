a7a10d1facc20e83c5f1ceaa00844c6b693a5fb9
kokarolos <kokarolos@gmail.com>
2020-08-14 10:37:15 +0300

    Cleaned

---
+++ Models/Concrete/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace Models
+{
+    public class Hero : Rectangle
+    {
+        public override Color Color { get; set; } = Color.Red;
+
+        public Hero()
+        {
+            GenerateHero();
+        }
+
+        public Hero(int Xposition, int Yposition) : base(Xposition, Yposition)
+        {
+            this.Xposition = Xposition;
+            this.Yposition = Xposition;
+        }
+
+        //Creates Initial Hero
+        private void GenerateHero()
+        {
+            for(int i =0; i<=4; i++)
+            {
+                rects.Add(new Rectangle(Xposition + i*10, Yposition + Width, Width, Height));
+            }
+        }
+    }
+}
---
+++ Models/Concrete/RhodeIslandZ.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace Models.Concrete
+{
+    public class RhodeIslandZ :Rectangle
+    {
+        public override int Yposition { get; set; } = 150;
+        public override Color Color { get; set; } = Color.Cyan;
+        public RhodeIslandZ()
+        {
+            GenerateClevelandZ();
+        }
+
+        public RhodeIslandZ(int Xposition, int Yposition) : base(Xposition, Yposition)
+        {
+            this.Xposition = Xposition;
+            this.Yposition = Xposition;
+        }
+        //Creates Initial 
+        private void GenerateClevelandZ()
+        {
+            rects.Add(new Rectangle(Xposition, Yposition, Width, Height));
+            rects.Add(new Rectangle(rects[0].Xposition + Width , Yposition, Width, Height));
+            rects.Add(new Rectangle(Xposition, rects[0].Yposition + Width, Height, Width));
+            rects.Add(new Rectangle(rects[2].Xposition-Width, rects[2].Yposition, Height, Width));
+        }
+
+    }
+}
